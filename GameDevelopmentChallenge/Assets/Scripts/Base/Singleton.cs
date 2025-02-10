c694b669c69f0b73c98ec8c9b88746dd270085cf
Nurullah Mert GULER <nmert06guler@gmail.com>
2025-02-10 14:13:12 +0300

    scene light settings

---
+++ GameDevelopmentChallenge/Assets/Scripts/Base/Singleton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+    }
+}
---
+++ GameDevelopmentChallenge/Assets/Scripts/Managers/GameVariables.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables : Singleton<GameVariables>
+{
+    [Space(5)]
+    public Transform soldierHolder;
+}
